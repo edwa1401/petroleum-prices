934d80d20721529daa63854227d96c9b3b1d1cbe
edwa1401 <edwa14011977@gmail.com>
2024-01-06 21:23:54 +0500

    fix admin

--- prices_analyzer/admin.py
+++ prices_analyzer/admin.py
@@ -3,8 +3,9 @@ from prices_analyzer.models import ProductionPlace, Basis, Depot
 
 
 class ProdictionPlaceAdmin(admin.ModelAdmin):
-    list_display = ('basis_code', 'basis_name', 'rzd_code_code', 'rzd_code_station_name', 'name')
-    list_filter = ('basis_name', 'rzd_code_station_name', 'name')
+    # list_display = ('basis_code', 'basis_name', 'rzd_code_code', 'rzd_code_station_name', 'name')
+    # list_filter = ('basis_name', 'rzd_code_station_name', 'name')
+    pass
 
 
 admin.site.register(ProductionPlace, ProdictionPlaceAdmin)
