fee8d54bffababa6e9661aab0aa1ebd323876bfe
edwa1401 <edwa14011977@gmail.com>
2024-01-27 13:39:22 +0500

    fixed mypy and workflows

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
 from rest_api import views
-from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('petroleum_maps/', views.PetroleumMapList.as_view()),
     path('petroleum_maps/<int:pk>/', views.PetroleumMapDetail.as_view()),
 ]
 
-
-urlpatterns = format_suffix_patterns(urlpatterns)
