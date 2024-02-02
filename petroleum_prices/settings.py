1610a24c37c14d4e71a3ceb9d1ee73418eecfe45
edwa1401 <edwa14011977@gmail.com>
2024-02-02 19:17:17 +0500

    set DEBUG to false for production

--- petroleum_prices/settings.py
+++ petroleum_prices/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default=get_random_secret_key())
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='localhost').split(',')
 
