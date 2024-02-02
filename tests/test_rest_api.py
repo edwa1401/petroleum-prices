c31f636bef7a78ef1d14b2d543cd3e188bbb705e
edwa1401 <edwa14011977@gmail.com>
2024-02-02 23:52:32 +0500

    add mark to test

--- tests/test_rest_api.py
+++ tests/test_rest_api.py
@@ -33,7 +33,7 @@ pytestmark = pytest.mark.django_db
 #     response = admin_client.get('/admin/')
 #     assert response.status_code == 200
 
-
+@pytest.mark.django_db
 def test_path_to_parser_without_day(client):
     response = client.get('/parser/')
     assert response.content == b'No day in request'
