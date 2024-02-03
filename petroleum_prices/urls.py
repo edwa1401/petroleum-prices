43d5156301d311d95f9ee6747f63abbb6f0cdb07
edwa1401 <edwa14011977@gmail.com>
2024-02-03 16:02:24 +0500

    changed url

--- petroleum_prices/urls.py
+++ petroleum_prices/urls.py
@@ -14,8 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
+# from django.conf import settings
+# from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import permissions
@@ -50,4 +50,6 @@ urlpatterns = [
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+# ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
