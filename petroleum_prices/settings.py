782dbd83ebdd8806f8a6fc0d01b0110eb168ae06
edwa1401 <edwa14011977@gmail.com>
2024-04-03 10:42:21 +0500

    fixed minor misstake

--- petroleum_prices/settings.py
+++ petroleum_prices/settings.py
@@ -178,7 +178,7 @@ CELERY_TASK_TIME_LIMIT = 30 * 60
 CELERY_RESULT_BACKEND = 'django-db'
 CELERY_CACHE_BACKEND = 'django-cache'
 CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
-CELERYD_OPTS="--time-limit==3600 -E --loglevel=DEBUG"
+CELERYD_OPTS = "--time-limit==3600 -E --loglevel=DEBUG"
 
 
 LOGGING = {
